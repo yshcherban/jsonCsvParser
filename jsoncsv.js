da555fad6ab633c5a0f8f96ad98fe72002580f69
Yaroslav Shcherban <yar.shcherban@gmail.com>
2017-03-13 17:32:04 +0200

    func separated

--- jsoncsv.js
+++ jsoncsv.js
@@ -6,189 +6,226 @@ const   fs      = require('fs'),
         Promise	= require('bluebird');
 
 
-const readJsonFile = Promise.promisify(fs.readFile);
-const readXlsxFile = Promise.method(XLSX.readFile);
-
-const parseCSVFile = function(file, config) {
-	return new Promise((resolve, reject) => {
-		const effectiveConfig = Object.assign({}, config, {
-			complete:	(results, file) => { resolve(results) },
-			error: 		(error, file) => { reject(error) }
-		});
-		baby.parseFiles(file, effectiveConfig);
-		// no return
-	});
-};
-
-/** possible values for well-known params */
-const guessTable = {
-	firstName:	['name', 'firstname'],
-	lastName:	['surname', 'lastname'],
-	gender: 	['gender'],
-	birthday: 	['birthday', 'bday', 'dob'],
-	form: 		['form'],
-	house: 		['house']
-};
-
-/**
- * Guess actual column name based on guessTable.
- * @param {Array.<String>} fieldNames array of actual name of columns
- * @param {String} fieldToGuess name of column to search. This is key in guess table. Like `firstName`
- * @return {String|undefined} actual name for `fieldToGuess`
- */
-const guessColumn = function(fieldNames, fieldToGuess) {
-	const possibleValues = guessTable[fieldToGuess];
-	if(!Array.isArray(possibleValues)) return undefined;	// there is no such value in table
+// Returns json file and origHeaders (headers from parsed file)
+class Parser {
+    constructor(file) {
+        this.file = file;
+        this.parsedFile;
+        this.origHeaders;
+    }
     
-	return fieldNames.find( fieldName => {
-		const lowFieldName = fieldName.toLowerCase();
-		return possibleValues.findIndex( possibleValue => possibleValue === lowFieldName ) !== -1;
-	});
-};
-
-/**
- * Return mapping object. Each key have real column name or undefined if no suitable column found
- * @param {Array.<String>} fieldNamesArray fieldNames array of actual name of columns
- * @return {{firstName: (String|undefined), lastName: (String|undefined), gender: (String|undefined), birthday: (String|undefined), form: (String|undefined), house: (String|undefined)}}
- */
-const guessHeaders = function(fieldNamesArray) {
-	return {
-		firstName:	guessColumn(fieldNamesArray, 'firstName'),
-		lastName:	guessColumn(fieldNamesArray, 'lastName'),
-		gender:		guessColumn(fieldNamesArray, 'gender'),
-		birthday:	guessColumn(fieldNamesArray, 'birthday'),
-		form:		guessColumn(fieldNamesArray, 'form'),
-		house:		guessColumn(fieldNamesArray, 'house')
-	}
-};
-
-/** Try to guess gender value */
-const guessGender = function (genderValue) {
-	const lowGender = genderValue.toLowerCase();
-    
-	if(lowGender === 'boy' || lowGender === 'male' || lowGender === '1') 	return 'MALE';
-	if(lowGender === 'girl' || lowGender === 'female' || lowGender === '0') return 'FEMALE';
-	
-    return genderValue;
-};
-
-const objectToStudent = function(headers, obj) {
-	const 	firstName	= obj[headers.firstName],
-			lastName	= obj[headers.lastName],
-			gender		= obj[headers.gender];
-            
-	return {
-		firstName:	firstName ? firstName.trim() : undefined,
-		lastName:	lastName ? lastName.trim() : undefined,
-		gender: 	gender ? guessGender(gender) : undefined,
-		
-        birthday:	obj[headers.birthday],
-		form:		obj[headers.form],
-		house: 		obj[headers.house]
-	};
-};
-
-/**
- * Casts the data to common structure before save it to DB
- */
-function getStudentsBeforeInsertIntoDb (data, headers) {
-    const studentArray = data.filter( item => Object.keys(item).length > 1 )	// removing empty objects
-			.map( item => objectToStudent(headers, item));
-        
-    console.log(studentArray);
-}
-
-
-/**
- * Reads students from CSV file
- */
-function readStudentsFromCSVFile(file) {
-    
-	return parseCSVFile(file, { header: true }).then( result => {
-		const 	data			= result.data || [],
-				origHeaders		= result.meta.fields || [],
-				guessedHeaders	= guessHeaders(origHeaders);
-        
-        getStudentsBeforeInsertIntoDb(data, guessedHeaders);
-		
-	}).catch(function(e) {
-        console.log("Error reading file", e);
-    });
-}
-
-/**
- * Reads students from JSON file
- */
-function readStudentsFromJSONFile(file) {
-    return readJsonFile(file, "utf8").then( contents => {
-        
-        const   getJsonFromfileContent = JSON.parse(contents);
-        let origHeaders = [];
-        
-        for (let i = 0; i < getJsonFromfileContent.length; i++) {
-            for (let cap in getJsonFromfileContent[i]) {
-                origHeaders.push(cap);
-            }
-            const guessedHeaders	= guessHeaders(origHeaders);
-            getStudentsBeforeInsertIntoDb([getJsonFromfileContent[i]], guessedHeaders);
-            origHeaders = [];
+    getTypeOfFile() {
+        switch(true) {
+            case (path.extname(this.file) === '.json') || (mime.lookup(this.file) === 'application/json'):
+                return 'json';
+                break;
+            case (path.extname(this.file) === '.csv') || (mime.lookup(this.file) === 'text/csv'):
+                return 'csv';
+                break;
+            case (path.extname(this.file) === '.xlsx') || (mime.lookup(this.file) === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'):
+                return 'xlsx';
+                break;
         }
+    }
+    
+    parse() {
+        switch(this.getTypeOfFile(this.file)) {
+            case 'json':
+                return this.readJsonFile(this.file, "utf8").then( contents => {
+                    // надо подумать
+                    /*this.parsedFile = JSON.parse(contents);
+                    let origHeaders = [];
+                    
+                    for (let i = 0; i < getJsonFromfileContent.length; i++) {
+                        for (let cap in getJsonFromfileContent[i]) {
+                            origHeaders.push(cap);
+                        }
+                        const guessedHeaders	= guessHeaders(origHeaders);
+                        getStudentsBeforeInsertIntoDb([getJsonFromfileContent[i]], guessedHeaders);
+                        origHeaders = [];
+                    }*/
+                });
+                
+                break;
+            case 'csv':
+                return this.parseCSVFile(this.file, { header: true }).then( result => {
+                    this.parsedFile = result.data || [];
+                    this.origHeaders = result.meta.fields || [];
+                });
+                break;
+            case 'xlsx':
+                Parser.readXlsxFile(this.file).then(readFile => {
+                    this.parsedFile = XLSX.utils.sheet_to_json(readFile.Sheets[readFile.SheetNames[0]]) || [];
+                    this.origHeaders = XLSX.utils.sheet_to_json(readFile.Sheets[readFile.SheetNames[0]], {header: 1})[0] || [];
+                });
+            break;
+        }
+    }
+    
+    /* getters */
+    get parsedFromOriginalFile() {
+        this.parsedFile;
+    }
+    
+    get parsedOriginalHeadersFromFile() {
+        return this.origHeaders;
+    }
+    
+    /* constants */
+    static get readJsonFile() {
+      return Promise.promisify(fs.readFile);
+    }
+    
+    static get readXlsxFile() {
+      return Promise.method(XLSX.readFile);
+    }
+    
+    static get effectiveConfig() {
+        return Object.assign({}, config, {
+                complete:	(results, file) => { resolve(results) },
+                error: 		(error, file) => { reject(error) }
+            });
+    }
+    
+    parseCSVFile(file, config) {
+        return new Promise((resolve, reject) => {
+            baby.parseFiles(file, Parser.effectiveConfig);
+            // no return
+        });
+    }
+    
+}
+
+
+// Returns data/file according to defined structure
+class DbPreparer {
+    constructor(fileContent, origHeaders) {
+        this.parsedFile = fileContent;
+        this.originalHeaders = origHeaders;
+    }
+    
+    
+    /** possible values for well-known params */
+    static get guessTable() {
+      return {
+            firstName:	['name', 'firstname'],
+            lastName:	['surname', 'lastname'],
+            gender: 	['gender'],
+            birthday: 	['birthday', 'bday', 'dob'],
+            form: 		['form'],
+            house: 		['house']
+        };  
+    } 
+    
+    /**
+     * Guess actual column name based on guessTable.
+     * @param {Array.<String>} fieldNames array of actual name of columns
+     * @param {String} fieldToGuess name of column to search. This is key in guess table. Like `firstName`
+     * @return {String|undefined} actual name for `fieldToGuess`
+     */
+    guessColumn(fieldNames, fieldToGuess) {
+        if(!Array.isArray(DbPreparer.possibleValues(fieldToGuess))) return undefined;	// there is no such value in table
         
-    }).catch(function(e) {
-        console.log("Error reading file", e);
-    });
-}
-
-/**
- * Reads students from XLSX file
- */
-function readStudentsFromXlsxFile(file) {
-    return readXlsxFile(file).then(readFile => {
-        const   workbook = readFile,
-                sheetNameList = workbook.SheetNames,
-                getJsonFromSheetNameList = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]]),
-                origHeaders		= XLSX.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]], {header: 1})[0] || [],
-                guessedHeaders	= guessHeaders(origHeaders);
+        return fieldNames.find( fieldName => {
+            return DbPreparer.possibleValues.findIndex( possibleValue => possibleValue === DbPreparer.lowFieldName(fieldName) ) !== -1;
+        });
+    };
+    
+    static set possibleValues(value) {
+        return DbPreparer.guessTable[value];
+    }
+    
+    static set lowFieldName(value) {
+        return value.toLowerCase();
+    }
+    
+    /**
+     * Return mapping object. Each key have real column name or undefined if no suitable column found
+     * @param {Array.<String>} fieldNamesArray fieldNames array of actual name of columns
+     * @return {{firstName: (String|undefined), lastName: (String|undefined), gender: (String|undefined), birthday: (String|undefined), form: (String|undefined), house: (String|undefined)}}
+     */
+    static set guessHeaders(fieldNamesArray) {
+        return {
+            firstName:	this.guessColumn(fieldNamesArray, 'firstName'),
+            lastName:	this.guessColumn(fieldNamesArray, 'lastName'),
+            gender:		this.guessColumn(fieldNamesArray, 'gender'),
+            birthday:	this.guessColumn(fieldNamesArray, 'birthday'),
+            form:		this.guessColumn(fieldNamesArray, 'form'),
+            house:		this.guessColumn(fieldNamesArray, 'house')
+        }
+    }
+    
+    /** Try to guess gender value */
+    static set guessGender(genderValue) {
+        
+        if(DbPreparer.lowGender(genderValue) === 'boy' || DbPreparer.lowGender(genderValue) === 'male' || DbPreparer.lowGender(genderValue) === '1') 	return 'MALE';
+        if(DbPreparer.lowGender(genderValue) === 'girl' || DbPreparer.lowGender(genderValue) === 'female' || DbPreparer.lowGender(genderValue) === '0') return 'FEMALE';
+        
+        return genderValue;
+    };
+    
+    static set lowGender(value) {
+        return value.toLowerCase();
+    }
+    
+    firstName(headers, obj) {
+        return obj[headers.firstName];
+    }
+    
+    lastName(headers, obj) {
+        return obj[headers.lastName];
+    }
+    
+    gender(headers, obj) {
+        return obj[headers.gender];
+    }
+    
+    objectToStudent (headers, obj) {
+        return {
+            firstName:	this.firstName(headers, obj) ? this.firstName(headers, obj).trim() : undefined,
+            lastName:	this.lastName(headers, obj) ? this.lastName(headers, obj).trim() : undefined,
+            gender: 	this.gender(headers, obj) ? DbPreparer.guessGender(this.gender(headers, obj)) : undefined,
             
-            getStudentsBeforeInsertIntoDb(getJsonFromSheetNameList, guessedHeaders);
-    }).catch(function(e) {
-        console.log("Error reading file", e);
-    });
-}
-
-/**
- * Gets passed file type
- */
-function getTypeOfFile(file) {
-    switch(true) {
-        case (path.extname(file) === '.json') || (mime.lookup(file) === 'application/json'):
-            return 'json';
-            break;
-        case (path.extname(file) === '.csv') || (mime.lookup(file) === 'text/csv'):
-            return 'csv';
-            break;
-        case (path.extname(file) === '.xlsx') || (mime.lookup(file) === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'):
-            return 'xlsx';
-            break;
+            birthday:	obj[headers.birthday],
+            form:		obj[headers.form],
+            house: 		obj[headers.house]
+        };
+    };
+    
+    getGuessedHeaders() {
+        return DbPreparer.guessHeaders(this.originalHeaders);
+    }
+    
+    getPreparedData() {
+        console.log(this.preparedData);
+    }
+    
+    static get preparedData() {
+        return this.parsedFile.filter( item => Object.keys(item).length > 1 )	// removing empty objects
+			.map( item => objectToStudent(this.getGuessedHeaders(), item));
     }
 }
+
+
+
+let parser = new Parser('students.xlsx');
+parser.parse();
+//console.log(parser.parsedFromOriginalFile);
+
+//let preparer = new DbPreparer(parser.parsedFromOriginalFile, parser.parsedOriginalHeadersFromFile);
+//preparer.getPreparedData();
+
+
+
 
 /**
  * Processes passed command line arguments
  */
-process.argv.forEach(function (val, index, array) {
-  switch (getTypeOfFile(val)) {
-    case 'json':
-        readStudentsFromJSONFile(val); 
-        break;
-    case 'csv':
-        readStudentsFromCSVFile(val);
-        break;
-    case 'xlsx':
-        readStudentsFromXlsxFile(val);
-        break;
-  }
-});
+/*process.argv.forEach(function (val, index, array) {
+    
+});*/
+
+
 
 
 
