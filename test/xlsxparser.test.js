761c6187dc5a2a3aed7ea33a0fb4da3ee8f5ad28
Yaroslav Shcherban <yar.shcherban@gmail.com>
2017-03-17 09:14:24 +0200

    xlsx test added

---
+++ test/xlsxparser.test.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const   chai = require('chai'),
+        should = chai.should(),
+        xlsxParser = require('../Xlsxparser');
+
+describe('XLSXparser', () => {
+    it('should consist the same pair-values"', (done) => {
+        xlsxParser.parse('students.xlsx').then( (res) => {
+            res.should.deep.equal([{
+                    "firstname": "Yaroslav",
+                    "lastname": "Shcherban",
+                    "gender": "boy"
+                },
+                {
+                    "firstname": "Ivan",
+                    "lastname": "Onoprienko",
+                    "gender": "male"
+                },
+                {
+                    "firstname": "Luiza",
+                    "lastname": "Smith",
+                    "gender": "girl"
+                },
+                {
+                    "firstname": "Igor",
+                    "lastname": "Kolomoyskij",
+                    "gender": "1"
+                },
+                {
+                    "firstname": "Natalia",
+                    "lastname": "Stashevskaja",
+                    "gender": "0"
+                }]
+            );
+            done();
+        });
+    });
+
+    it('should return error for wrong file type', (done) => {
+        xlsxParser.parse('students.csv').catch( e => {
+            done();
+        });
+    });
+
+    it('should return error of broken file', (done) => {
+        xlsxParser.parse('students2.xlsx').catch( e => {
+            done();
+        });
+    });
+
+});
