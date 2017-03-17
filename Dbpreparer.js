0e18677081c60f031afd92f5dbfc4a353bad85c6
Yaroslav Shcherban <yar.shcherban@gmail.com>
2017-03-17 16:35:36 +0200

    dbpreparer as promise

--- Dbpreparer.js
+++ Dbpreparer.js
@@ -1,112 +1,92 @@
-class DbPreparer {
-    constructor() {
-        /** possible values for well-known params */
-        this.guessTable = {
-            firstName:	['name', 'firstname'],
-            lastName:	['surname', 'lastname'],
-            gender: 	['gender'],
-            birthday: 	['birthday', 'bday', 'dob'],
-            form: 		['form'],
-            house: 		['house']
-        };
+const   Promise	= require('bluebird');
+
+function isJson(jsonObj) {
+    try {
+        JSON.parse(JSON.stringify(jsonObj));
+    } catch (e) {
+        return false;
     }
-
-    /**
-     * Guess actual column name based on guessTable.
-     * @param {Array.<String>} fieldNames array of actual name of columns
-     * @param {String} fieldToGuess name of column to search. This is key in guess table. Like `firstName`
-     * @return {String|undefined} actual name for `fieldToGuess`
-     */
-    guessColumn(fieldNames, fieldToGuess) {
-        if(!Array.isArray(this.possibleValues(fieldToGuess))) return undefined;	// there is no such value in table
-
-        return fieldNames.find( fieldName => {
-            return this.possibleValues(fieldToGuess).findIndex( possibleValue => possibleValue === this.lowFieldName(fieldName) ) !== -1;
-        });
-    };
-
-    possibleValues(value) {
-        return this.guessTable[value];
-    }
-
-    lowFieldName(value) {
-        return value.toLowerCase();
-    }
-
-    /**
-     * Return mapping object. Each key have real column name or undefined if no suitable column found
-     * @param {Array.<String>} fieldNamesArray fieldNames array of actual name of columns
-     * @return {{firstName: (String|undefined), lastName: (String|undefined), gender: (String|undefined), birthday: (String|undefined), form: (String|undefined), house: (String|undefined)}}
-     */
-    guessHeaders(fieldNamesArray) {
-        return {
-            firstName:	this.guessColumn(fieldNamesArray, 'firstName'),
-            lastName:	this.guessColumn(fieldNamesArray, 'lastName'),
-            gender:		this.guessColumn(fieldNamesArray, 'gender'),
-            birthday:	this.guessColumn(fieldNamesArray, 'birthday'),
-            form:		this.guessColumn(fieldNamesArray, 'form'),
-            house:		this.guessColumn(fieldNamesArray, 'house')
-        }
-    }
-
-    /** Try to guess gender value */
-    guessGender(genderValue) {
-
-        if(this.lowGender(genderValue) === 'boy' || this.lowGender(genderValue) === 'male' || this.lowGender(genderValue) === '1') 	return 'MALE';
-        if(this.lowGender(genderValue) === 'girl' || this.lowGender(genderValue) === 'female' || this.lowGender(genderValue) === '0') return 'FEMALE';
-
-        return genderValue;
-    };
-
-    lowGender(value) {
-        return value.toLowerCase();
-    }
-
-    firstName(headers, obj) {
-        return obj[headers.firstName];
-    }
-
-    lastName(headers, obj) {
-        return obj[headers.lastName];
-    }
-
-    gender(headers, obj) {
-        return obj[headers.gender];
-    }
-
-    objectToStudent (headers, obj) {
-        return {
-            firstName:	this.firstName(headers, obj) ? this.firstName(headers, obj).trim() : undefined,
-            lastName:	this.lastName(headers, obj) ? this.lastName(headers, obj).trim() : undefined,
-            gender: 	this.gender(headers, obj) ? this.guessGender(this.gender(headers, obj)) : undefined,
-
-            birthday:	obj[headers.birthday],
-            form:		obj[headers.form],
-            house: 		obj[headers.house]
-        };
-    };
-
-    getPreparedData(objJson) {
-        const preparedJson = [];
-        let origHeaders = [];
-
-        for (let i = 0; i < objJson.length; i++) {
-            for (let cap in objJson[i]) {
-                origHeaders.push(cap);
-            }
-
-            const guessedHeaders = this.guessHeaders(origHeaders);
-            preparedJson.push(this.preparedData([objJson[i]], guessedHeaders));
-            origHeaders = [];
-        }
-
-        return preparedJson;
-    }
-
-    preparedData(data, headers) {
-        return data.filter( item => Object.keys(item).length > 1 )	// removing empty objects
-            .map( item => this.objectToStudent(headers, item));
-    }
+    return true;
 }
 
-module.exports = new DbPreparer();
+function getPreparedData ( jsonObj ) {
+    return new Promise((resolve, reject) => {
+        let origHeaders = [];
+
+        for (let i = 0; i < jsonObj.length; i++) {
+            for (let cap in jsonObj[i]) {
+                origHeaders.push(cap);
+            }
+        }
+        if (origHeaders.length > 0) {
+            resolve(origHeaders);
+        } else {
+            reject(new Error('not found'));
+        }
+
+    }).then( origHeader => {
+        const guessColumn = function(origHeader, fieldToGuess) {
+
+            /** possible values for well-known params */
+            const guessTable = {
+                firstName:	['name', 'firstname'],
+                lastName:	['surname', 'lastname'],
+                gender: 	['gender'],
+                birthday: 	['birthday', 'bday', 'dob'],
+                form: 		['form'],
+                house: 		['house']
+            };
+
+            const possibleValues = guessTable[fieldToGuess];
+            if(!Array.isArray(possibleValues)) throw new Error("no such value in table"); //return undefined;	// there is no such value in table
+
+            return origHeader.find( fieldName => {
+                const lowFieldName = fieldName.toLowerCase();
+                return possibleValues.findIndex( possibleValue => possibleValue === lowFieldName ) !== -1;
+            });
+        }
+
+        return {
+            firstName:	guessColumn(origHeader, 'firstName'),
+            lastName:	guessColumn(origHeader, 'lastName'),
+            gender:		guessColumn(origHeader, 'gender'),
+            birthday:	guessColumn(origHeader, 'birthday'),
+            form:		guessColumn(origHeader, 'form'),
+            house:		guessColumn(origHeader, 'house')
+        }
+
+    }).then( guessedHeaders => {
+        /** Try to guess gender value */
+        const guessGender = function (genderValue) {
+            const lowGender = genderValue.toLowerCase();
+
+            if(lowGender === 'boy' || lowGender === 'male' || lowGender === '1') 	return 'MALE';
+            if(lowGender === 'girl' || lowGender === 'female' || lowGender === '0') return 'FEMALE';
+
+            return genderValue;
+        };
+
+        const objectToStudent = function(headers, obj) {
+            const 	firstName	= obj[headers.firstName],
+                lastName	= obj[headers.lastName],
+                gender		= obj[headers.gender];
+
+            return {
+                firstName:	firstName ? firstName.trim() : undefined,
+                lastName:	lastName ? lastName.trim() : undefined,
+                gender: 	gender ? guessGender(gender) : undefined,
+
+                birthday:	obj[headers.birthday],
+                form:		obj[headers.form],
+                house: 		obj[headers.house]
+            };
+        };
+
+        return jsonObj.filter( item => Object.keys(item).length > 1 )	// removing empty objects
+            .map( item => objectToStudent(guessedHeaders, item));
+    });
+}
+
+
+module.exports.isJson = isJson;
+module.exports.getPreparedData = getPreparedData;
--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const jsonParser = require('./Jsonparser');
 const csvParser = require('./Csvparser');
 const xlsxParser = require('./Xlsxparser');
+const dbpreparer = require('./Dbpreparer');
 
 
-// jsonParser.parse('stud.json').then(res => {
-//     console.log(res);
-// });
-
-// csvParser.parse('students.csv').then(res => {
-//     console.log(res);
-// });
-
-xlsxParser.parse('students.xlsx').then(res => {
-    console.log(res);
+jsonParser.parse('stud.json').then(res => {
+    dbpreparer.getPreparedData(res).then(res => {
+        console.log(res);
+    });
 });
+
+// csvParser.parse('students-broken.csv').then(res => {
+//     console.log(res);
+// });
+
+// xlsxParser.parse('students2.xlsx').then(res => {
+//     console.log(res);
+// });
