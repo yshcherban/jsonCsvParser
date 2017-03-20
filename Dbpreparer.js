5f5f6c5529f08a3223a05f58f1b44bdf76bcf9b3
Yaroslav Shcherban <yar.shcherban@gmail.com>
2017-03-20 13:29:49 +0200

    req field

--- Dbpreparer.js
+++ Dbpreparer.js
@@ -9,82 +9,116 @@ function isJson(jsonObj) {
     return true;
 }
 
-function getPreparedData ( jsonObj ) {
-    return new Promise((resolve, reject) => {
-        let origHeaders = [];
+function getPreparedData ( arrJsonObj ) {
+    let preparedData = [];
+    let unpreparedData = [];
+    const required = ["firstName", "lastName", "gender"];
 
-        for (let i = 0; i < jsonObj.length; i++) {
-            for (let cap in jsonObj[i]) {
+    for (let i = 0; i < arrJsonObj.length; i++) {
+
+        const promiseResponse = new Promise((resolve, reject) => {
+            let origHeaders = [];
+
+
+            for (let cap in arrJsonObj[i]) {
                 origHeaders.push(cap);
             }
-        }
-        if (origHeaders.length > 0) {
-            resolve(origHeaders);
-        } else {
-            reject(new Error('not found'));
-        }
 
-    }).then( origHeader => {
-        const guessColumn = function(origHeader, fieldToGuess) {
+            if (origHeaders.length > 0) {
+                resolve(origHeaders);
+            } else {
+                reject(new Error('not found'));
+            }
 
-            /** possible values for well-known params */
-            const guessTable = {
-                firstName:	['name', 'firstname'],
-                lastName:	['surname', 'lastname'],
-                gender: 	['gender'],
-                birthday: 	['birthday', 'bday', 'dob'],
-                form: 		['form'],
-                house: 		['house']
-            };
+        }).then(origHeader => {
+            const guessColumn = function (origHeader, fieldToGuess) {
 
-            const possibleValues = guessTable[fieldToGuess];
-            if(!Array.isArray(possibleValues)) throw new Error("no such value in table"); //return undefined;	// there is no such value in table
+                /** possible values for well-known params */
+                const guessTable = {
+                    firstName: ['name', 'firstname'],
+                    lastName: ['surname', 'lastname'],
+                    gender: ['gender'],
+                    birthday: ['birthday', 'bday', 'dob'],
+                    form: ['form'],
+                    house: ['house']
+                };
 
-            return origHeader.find( fieldName => {
-                const lowFieldName = fieldName.toLowerCase();
-                return possibleValues.findIndex( possibleValue => possibleValue === lowFieldName ) !== -1;
-            });
-        }
+                const possibleValues = guessTable[fieldToGuess];
+                if (!Array.isArray(possibleValues)) throw new Error("no such value in table"); //return undefined;	// there is no such value in table
 
-        return {
-            firstName:	guessColumn(origHeader, 'firstName'),
-            lastName:	guessColumn(origHeader, 'lastName'),
-            gender:		guessColumn(origHeader, 'gender'),
-            birthday:	guessColumn(origHeader, 'birthday'),
-            form:		guessColumn(origHeader, 'form'),
-            house:		guessColumn(origHeader, 'house')
-        }
-
-    }).then( guessedHeaders => {
-        /** Try to guess gender value */
-        const guessGender = function (genderValue) {
-            const lowGender = genderValue.toLowerCase();
-
-            if(lowGender === 'boy' || lowGender === 'male' || lowGender === '1') 	return 'MALE';
-            if(lowGender === 'girl' || lowGender === 'female' || lowGender === '0') return 'FEMALE';
-
-            return genderValue;
-        };
-
-        const objectToStudent = function(headers, obj) {
-            const 	firstName	= obj[headers.firstName],
-                lastName	= obj[headers.lastName],
-                gender		= obj[headers.gender];
+                return origHeader.find(fieldName => {
+                    const lowFieldName = fieldName.toLowerCase();
+                    return possibleValues.findIndex(possibleValue => possibleValue === lowFieldName) !== -1;
+                });
+            }
 
             return {
-                firstName:	firstName ? firstName.trim() : undefined,
-                lastName:	lastName ? lastName.trim() : undefined,
-                gender: 	gender ? guessGender(gender) : undefined,
+                firstName: guessColumn(origHeader, 'firstName'),
+                lastName: guessColumn(origHeader, 'lastName'),
+                gender: guessColumn(origHeader, 'gender'),
+                birthday: guessColumn(origHeader, 'birthday'),
+                form: guessColumn(origHeader, 'form'),
+                house: guessColumn(origHeader, 'house')
+            }
 
-                birthday:	obj[headers.birthday],
-                form:		obj[headers.form],
-                house: 		obj[headers.house]
+        }).then(guessedHeaders => {
+            /** Try to guess gender value */
+            const guessGender = function (genderValue) {
+                const lowGender = genderValue.toLowerCase();
+
+                if (lowGender === 'boy' || lowGender === 'male' || lowGender === '1')    return 'MALE';
+                if (lowGender === 'girl' || lowGender === 'female' || lowGender === '0') return 'FEMALE';
+
+                return genderValue;
             };
-        };
 
-        return jsonObj.filter( item => Object.keys(item).length > 1 )	// removing empty objects
-            .map( item => objectToStudent(guessedHeaders, item));
-    });
+            const objectToStudent = function (headers, obj) {
+                const   firstName = obj[headers.firstName],
+                        lastName = obj[headers.lastName],
+                        gender = obj[headers.gender];
+
+                return {
+                    firstName: firstName ? firstName.trim() : undefined,
+                    lastName: lastName ? lastName.trim() : undefined,
+                    gender: gender ? guessGender(gender) : undefined,
+
+                    birthday: obj[headers.birthday],
+                    form: obj[headers.form],
+                    house: obj[headers.house]
+                };
+            };
+
+            const jSonObj = [arrJsonObj[i]].filter(item => Object.keys(item).length > 1)	// removing empty objects
+                .map(item => objectToStudent(guessedHeaders, item));
+
+            return jSonObj[0];
+
+        }).then( prepObj => {
+            if (required.length > 0) {
+                let foundReqHeaders = 0;
+                required.forEach( (val, i, arr) => {
+                    if ((Object.keys(prepObj).indexOf(val)) !== -1) {
+                        if ( prepObj[val] !== undefined ) {
+                            foundReqHeaders += 1;
+                        }
+                    }
+                });
+
+                if (required.length === foundReqHeaders) {
+                   return prepObj;
+                }
+
+            } else {
+                return prepObj;
+            }
+
+        });
+
+
+        preparedData.push(promiseResponse);
+    }
+
+    return Promise.all(preparedData);
 }
 
 
